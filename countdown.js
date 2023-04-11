4a1e403bf6bfa7bdeaf9edbda74a8e2c48316d6f
arthurgbecker <arthurgbecker@gmail.com>
2023-04-11 17:50:30 -0300

    Commit CCXP

---
+++ countdown.js
@@ -0,0 +1,30 @@
+const dia = document.getElementById('dia')
+const hora = document.getElementById('hora')
+const minuto = document.getElementById('minuto')
+const segundo = document.getElementById('segundo')
+
+const evento = "01 dec 2023"
+
+function countDown(){
+    const dataEvento = new Date(evento)
+    const hoje = new Date()
+
+    const segTotal = (dataEvento - hoje)/1000;
+
+    const finalDias = Math.floor(segTotal / 60 / 60 / 24);
+    const finalHoras = Math.floor(segTotal / 60 / 60) % 24;
+    const finalMinutos = Math.floor(segTotal / 60) % 60;
+    const finalSegundos = Math.floor(segTotal) % 60;
+
+    dia.innerHTML = formatoTempo(finalDias)+"D"
+    hora.innerHTML = formatoTempo(finalHoras)+"H"
+    minuto.innerHTML = formatoTempo(finalMinutos)+"M"
+    segundo.innerHTML = formatoTempo(finalSegundos)+"S"
+}
+
+function formatoTempo(tempo){
+    return tempo < 10? `0${tempo}` : tempo;
+}
+
+countDown();
+setInterval(countDown, 1000);
